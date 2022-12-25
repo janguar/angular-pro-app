2be26c3710bd2a51c47b51693549358375c30fea
Victor Puscarschi <vikj93@live.it>
2022-12-25 16:14:03 +0100

    add 404 page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 const usersModule = () => import('./pages/users/users.module').then(x => x.UsersModule);
 
@@ -9,7 +10,7 @@ const routes: Routes = [
   { path: 'users', loadChildren: usersModule },
 
   // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+  { path: '**', component: Page404Component }
 ];
 
 @NgModule({
