77d9c778e1431c6d6678f0a4fe82ff7bfb865ccb
Victor Puscarschi <vikj93@live.it>
2023-01-02 21:47:33 +0100

    add error response

--- server/server.js
+++ server/server.js
@@ -42,24 +42,64 @@ cron.schedule('1 0 0 * * *',() => {
   console.log(Date.now(),' running a task every minute');
 });
 
+function InvalidIdException() {
+  this.status = 400;
+  this.message = 'Invalid ID';
+}
+
+function UserNotFoundException() {
+  this.status = 404;
+  this.message = 'User not found';
+}
 
 
 // api routes
 // app.use('/users',require('./controllers/users.controller'));
 
+/**
+ */
 app.post('/users',async (req,res) => {
   await User.create(req.body);
   res.send("success");
 })
 
+/**
+ */
 app.get('/users',async (req,res) => {
-  const users = await User.findAll();
-  res.send(users);
+  const pageAsNumber = Number.parseInt(req.query.page);
+  const sizeAsNumber = Number.parseInt(req.query.size);
+
+  let page = 0;
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+    page = pageAsNumber;
+  }
+
+  let size = 10;
+  if (!Number.isNaN(sizeAsNumber) && !(sizeAsNumber > 10) && !(sizeAsNumber < 1)) {
+    size = sizeAsNumber;
+  }
+
+  const usersWithCount = await User.findAndCountAll({
+    limit: size,
+    offset: page * size
+  });
+  res.send({
+    content: usersWithCount.rows,
+    totalPages: Math.ceil(usersWithCount.count / Number.parseInt(size))
+  });
 })
 
-app.get('/users/:id',async (req,res) => {
-  const id = req.params.id;
+/**
+ */
+app.get('/users/:id',async (req,res,next) => {
+  const id = Number.parseInt(req.params.id);
+  if (Number.isNaN(id)) {
+    next(new InvalidIdException());
+  }
   const user = await User.findOne({where: {id: id}});
+  if (!user) {
+    next(new UserNotFoundException());
+  }
   res.send(user);
 })
 
@@ -79,7 +119,18 @@ app.delete('/users/:id',async (req,res) => {
 
 
 // global error handler
-app.use(errorHandler);
+// app.use(errorHandler);
+
+app.use((err,req,res,next) => {
+  res
+    .status(err.status)
+    .send(
+      {
+        message: err.message,
+        timestamp: Date.now(),
+        path: req.originalUrl
+      });
+})
 
 
 
