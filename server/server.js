d88133e7cb19187193c7f7c2701fed0242db30c4
Victor Puscarschi <vikj93@live.it>
2023-01-02 21:36:47 +0100

    base user crud

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 require('rootpath')();
 const dotenv = require('dotenv');
 const express = require("express");
-var app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 // const basicAuth = require('./helpers/basic-auth');
 const errorHandler = require('./helpers/error-handler');
 const generateJwt = require('./helpers/jwt');
 const morgan = require('morgan');
-var cron = require('node-cron');
+const cron = require('node-cron');
 const sequelize = require('./helpers/database');
 const User = require('./models/user.model');
+const app = express();
 
+sequelize.sync(
+  {
+    force: true
+    // alter: true
+  }
+).then(async () => {
+  console.log("database ok!");
+  for (let i = 1; i <= 25; i++) {
+    const user = {
+      username: `user${i}`,
+      email: `user${i}@mail.com`,
+      password: 'P4ssword'
+    }
+    await User.create(user);
+  }
+})
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -26,72 +42,41 @@ cron.schedule('1 0 0 * * *',() => {
   console.log(Date.now(),' running a task every minute');
 });
 
-sequelize.sync(
-  {
-    // force: true,
-    alter: true
-  }
-).then(() => {
-  console.log("database ok!")
-})
 
-// authenticate with the database
-// database
-//   .sync()
-//   // .authenticate()
-//   // .sync()
-//   .then(function (err) {
-//     console.log("Connection established.");
-//   })
-//   .catch(function (err) {
-//     console.log("Unable to connect to database: ",err);
-//     // sequelize.close()
-//   });
-
-// // use basic HTTP auth to secure the api
-// app.use(basicAuth);
-
-// use JWT auth to secure the api
-// app.use(generateJwt());
 
 // api routes
-app.use('/users',require('./controllers/users.controller'));
+// app.use('/users',require('./controllers/users.controller'));
 
-app.use('/test/:id',async (req,res) => {
-  // create
-
-  // User.create(req.body).then(() => {
-  //   res.send(req.body + " is created!");
-  //   console.log(req.body," are inserted");
-  // }).catch(function (err) {
-  //   console.log("ERROR: ",err);
-  //   res.send("ERROR: ",err);
-  //   // sequelize.close()
-  // });
-
-  // get all
-
-  // const users = await User.findAll();
-  // // console.log(users);
-  // res.send(users);
-
-  // get by id
-
-  // const user = await User.findOne({where: {id: req.params.id}});
-  // res.send(user);
-
-
-  // update
-  // const user = await User.findOne({where: {id: req.params.id}});
-  // user.username = user.username + "UPDATED";
-  // res.send(user);
-  // await user.save();
-
-  // delete
-
-  await User.destroy({where: {id: req.params.id}});
-  res.send("Deleted");
+app.post('/users',async (req,res) => {
+  await User.create(req.body);
+  res.send("success");
 })
+
+app.get('/users',async (req,res) => {
+  const users = await User.findAll();
+  res.send(users);
+})
+
+app.get('/users/:id',async (req,res) => {
+  const id = req.params.id;
+  const user = await User.findOne({where: {id: id}});
+  res.send(user);
+})
+
+app.put('/users/:id',async (req,res) => {
+  const id = req.params.id;
+  const user = await User.findOne({where: {id: id}});
+  user.username = req.body.username;
+  await user.save();
+  res.send('updated');
+})
+
+app.delete('/users/:id',async (req,res) => {
+  const id = req.params.id;
+  await User.destroy({where: {id: id}});
+  res.send('removed');
+})
+
 
 // global error handler
 app.use(errorHandler);
